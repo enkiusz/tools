d6111ce314f6226459fec8cd84a2694e645fdb08
Maciej Grela <enki@fsck.pl>
2017-09-25 00:56:44 +0200

    gitup: Initial revision

---
+++ bin/gitup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+## Category: Daily/maintenance
+## Shortdesc: Pull from origin in all cloned repositories
+
+readonly REPOS_BASE="$HOME/repos"
+
+# Colors
+readonly BLACK=0 RED=1 GREEN=2 YELLOW=2 BLUE=4 MAGENTA=5 CYAN=6 WHITE=7
+
+titleon()
+{
+    tput setf $YELLOW
+}
+titleoff()
+{
+    tput sgr0
+}
+
+find "$REPOS_BASE" -name '.git' -type d | while read gitdir; do
+    repo_dir=$(dirname "$gitdir")
+    titleon; echo "--- Maintaining repo in '$repo_dir"; titleoff
+    pushd "$repo_dir" > /dev/null
+    git pull
+    popd > /dev/null
+done
+
