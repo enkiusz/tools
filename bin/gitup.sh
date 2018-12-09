6fb83e5e0abd7a630595026bd154ea836b61fffc
Maciej Grela <enki@fsck.pl>
2018-12-09 18:24:50 +0100

    gitup: Don't print the default message from git-pull

--- bin/gitup.sh
+++ bin/gitup.sh
@@ -21,7 +21,8 @@ find "$REPOS_BASE" -name '.git' -type d | while read gitdir; do
     repo_dir=$(dirname "$gitdir")
     titleon; echo "Maintaining repo in '$repo_dir'"; titleoff
     pushd "$repo_dir" > /dev/null
-    git pull
+    # Get rid of default message printed when remote repository was not changed.
+    git pull | grep -v -F -e 'Already up to date.'
     git status -s
     popd > /dev/null
 done
