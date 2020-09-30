379bd2c92016fbcf8c64703ab93d2587e80ca340
Maciej Grela <enki@fsck.pl>
2020-09-30 16:48:16 +0200

    bootstrap: Make bootstrapping on Darwin idempotent

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,7 +23,7 @@ if [ "$(uname)" == "Darwin" ]; then
 	echo "# Homebrew installation:"
 	echo "#"
 
-	# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+	which -s brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 	brew install stow
 
