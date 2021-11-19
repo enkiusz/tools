48e7736fe1b5873f7d0252fa9a7437b93d376fa0
Maciej Grela <enki@fsck.pl>
2021-11-19 16:22:58 +0100

    generate-README: Correctly handle files which do not have the README metadata

--- bin/generate-README.rb
+++ bin/generate-README.rb
@@ -7,9 +7,15 @@ categories = {}
 
 ARGV.each do |filename|
   File.open(filename) do |f|
-    cat = f.each_line.grep(/^## Category: .+/ ).first[/^## Category: (.+)/,1]
+    cat = f.each_line.grep(/^## Category: .+/ )
     f.seek(0)
-    desc = f.each_line.grep(/^## Shortdesc: .+/ ).first[/^## Shortdesc: (.+)/,1]
+    desc = f.each_line.grep(/^## Shortdesc: .+/ )
+    if !cat.first or !desc.first
+      next
+    end
+
+    cat = cat.first[/^## Category: (.+)/,1]
+    desc = desc.first[/^## Shortdesc: (.+)/,1]
     categories[cat] = [] if not categories.has_key?(cat)
     categories[cat].push({filename: filename, shortdesc: desc})
   end
